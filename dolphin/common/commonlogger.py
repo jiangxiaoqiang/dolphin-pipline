ed9701791f0f998425a24b2eeb555c627aeab44a
jiangxiaoqiang <jiangtingqiang@gmail.com>
2019-03-23 00:45:38 +0800

    add:add log rotate

--- dolphin/common/commonlogger.py
+++ dolphin/common/commonlogger.py
@@ -8,16 +8,15 @@ import time
 
 config = configparser.ConfigParser()
 configFilePath = os.path.abspath("dolphin/config.ini")
-with open(configFilePath, "r",encoding="utf-8") as cfgfile:
+with open(configFilePath, "r", encoding="utf-8") as cfgfile:
     config.readfp(cfgfile)
 
 
 class commonlogger:
     def __init__(self):
-        now_date_time = time.strftime('%Y-%m-%d',time.localtime())
+        now_date_time = time.strftime('%Y-%m-%d', time.localtime())
         logFileName = "./dolphin/log/spider" + now_date_time + ".log"
-        handler = logging.handlers.RotatingFileHandler(logFileName, maxBytes=1024 * 1024 * 1024,
-                                                       backupCount=5)
+        handler = logging.handlers.RotatingFileHandler(logFileName, maxBytes=1024*1024*50, backupCount=2)
         format = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
         formatter = logging.Formatter(format)  # 实例化formatter
         handler.setFormatter(formatter)  # 为handler添加formatter
--- manage.py
+++ manage.py
@@ -2,19 +2,16 @@
 
 import os
 import sys
-import logging
 
-logging.basicConfig(level=logging.DEBUG,#控制台打印的日志级别
-                    filename='new.log',
-                    filemode='a',##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
-                    #a是追加模式，默认如果不写的话，就是追加模式
-                    format=
-                    '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'
-                    #日志格式
-                    )
+from dolphin.common.commonlogger import commonlogger
+
+logger = commonlogger().getlogger()
+
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dolphin.settings')
     try:
+        while True:
+            logger.error("ddd")
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
