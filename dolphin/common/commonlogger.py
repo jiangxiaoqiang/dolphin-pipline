4663ac2fafd9f48dfb97f3bcd45d04fd01d81df4
jiangxiaoqiang <jiangtingqiang@gmail.com>
2019-03-23 19:12:12 +0800

    daily

--- dolphin/common/commonlogger.py
+++ dolphin/common/commonlogger.py
@@ -1,4 +1,4 @@
-# --encoding:utf-8--
+# -- encoding: utf-8 --
 
 import logging
 import configparser
@@ -11,17 +11,16 @@ configFilePath = os.path.abspath("dolphin/config.ini")
 with open(configFilePath, "r", encoding="utf-8") as cfgfile:
     config.readfp(cfgfile)
 
-
 class commonlogger:
     def __init__(self):
         now_date_time = time.strftime('%Y-%m-%d', time.localtime())
         logFileName = "./dolphin/log/spider" + now_date_time + ".log"
         handler = logging.handlers.RotatingFileHandler(logFileName, maxBytes=1024*1024*50, backupCount=2)
         format = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
-        formatter = logging.Formatter(format)  # 实例化formatter
-        handler.setFormatter(formatter)  # 为handler添加formatter
-        self.logger = logging.getLogger('tst')  # 获取名为tst的logger
-        self.logger.addHandler(handler)  # 为logger添加handler
+        formatter = logging.Formatter(format)
+        handler.setFormatter(formatter)
+        self.logger = logging.getLogger('tst')
+        self.logger.addHandler(handler)
         self.logger.setLevel(logging.DEBUG)
 
     def getlogger(self):
